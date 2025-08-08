1bcd0fd685510c91ba8af935c77d83cb79b5cc83
HalfAsianGuy <111540317+HalfAsianGuy@users.noreply.github.com>
2025-08-08 23:30:27 +0300

    add-all-files

---
+++ src/ru/store/online/model/enumeration/Country.java
@@ -0,0 +1,6 @@
+package ru.store.online.model.enumeration;
+
+public enum Country {
+
+    BRASILIA, SPAIN, INDIA, MEXICO, GEORGIA, CHINA, ARGENTINA
+}
---
+++ src/ru/store/online/model/enumeration/Status.java
@@ -0,0 +1,6 @@
+package ru.store.online.model.enumeration;
+
+public enum Status {
+
+    NEW, PAYED, CANCELLED, APPROVED, DONE
+}
